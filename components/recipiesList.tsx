d1426838c58f9df71ac0bdcd63f63d01421847ba
nimurarikuto <nimuyohu0105@gmail.com>
2021-03-26 00:02:44 +0900

    recipiesListのデザイン変更

--- components/recipiesList.tsx
+++ components/recipiesList.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import type { Recipe } from '../lib/recipe'
-import { Row, Col, Card} from 'react-bootstrap';
+import { Row, Col, Card, CardDeck} from 'react-bootstrap';
 import Link from 'next/link';
 
 type Props = {
@@ -11,36 +11,34 @@ export const RecipesList: FC<Props>= (props) => {
     const  {recipes} = props
     return (
         <ul style={{padding: '0'}}>
-                {recipes.map((recipe,index) => (
-                    <li style={{listStyle: 'none'}} key={index} className='mb-3'>
-                        <Row>
-                            <Col >
-                                <Card>
-                                {recipe.image_url ? (
+            <CardDeck>
+            {recipes.map((recipe,index) => (
+                <Col md={6} style={{padding: '0'}}>
+                <Card key={index}>
+                {recipe.image_url ? (
                                     <Link href={`recipes/${recipe.id}`}>
                                         <Card.Img variant="top" src={recipe.image_url} />
                                     </Link>
                                     ) : (
-                                        <Link href={`recipes/${recipe.id}`}>
-                                                <Card.Img variant="top" src='/images/noimage.png' />
-                                        </Link>
-                                        )}
-                                <Card.Body>
                                     <Link href={`recipes/${recipe.id}`}>
-                                        <Card.Title>{recipe.title}</Card.Title>
+                                        <Card.Img variant="top" src='/images/noimage.png' />
                                     </Link>
-                                    <Card.Text>
-                                    {recipe.description}
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">{recipe.published_at}</small>
-                                </Card.Footer>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </li>
-                ))}
+                )}
+                <Card.Body>
+                    <Link href={`recipes/${recipe.id}`}>
+                    <Card.Title>{recipe.title}</Card.Title>
+                    </Link>
+                    <Card.Text>
+                        {recipe.description}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">{recipe.published_at}</small>
+                </Card.Footer>
+                </Card>
+                </Col>
+            ))}
+            </CardDeck>
             </ul>
     )
 
