0bc900efeaea62e299ea012719c6ff4b601cc939
nimurarikuto <nimuyohu0105@gmail.com>
2021-03-25 17:00:42 +0900

    HeaderコンポーネントTypeScript化

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,8 @@
 import { Navbar } from 'react-bootstrap';
 import Link from 'next/link';
+import { FC } from "react";
 
-export const Header = () => {
+export const Header: FC = () => {
     return (
         <Navbar bg="dark" variant="dark">
                 <Link href='/'><Navbar.Brand href="/">レシピサイト🍳~料理が楽しい生活を~</Navbar.Brand></Link>
